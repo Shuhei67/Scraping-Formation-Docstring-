79419cb7b2fd8f29a9ffe7e3c82d1bca159fc95f
Lucas <lucas.hoffart2002@gmail.com>
2026-03-10 02:59:54 +0100

    Premier gros exercice

---
+++ Part-1/Exercice-3.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://books.toscrape.com/"
+response = requests.get(url)
+
+with open("index.html", "w") as f:
+    f.write(response.text)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+liste_categories = soup.find("aside").find("div", class_ = "side_categories").find("ul").find("li").find("ul")
+for category in liste_categories.children:
+    if category.name:
+        if category.a:
+            page_categorie = requests.get(url + category.a["href"])
+            contenue_page_categorie = BeautifulSoup(page_categorie.text, "html.parser")
+            articles = contenue_page_categorie.find("section").find_all("article")
+            if len(articles) < 20:
+                if len(articles) < 5:
+                    print(category.text.strip())
+
+
+# Récupère la page d'accueil de books.toscrape.com
+# Extrait la liste des catégories depuis la barre latérale
+# Pour chaque catégorie :
+#   - Visite sa page
+#   - Compte le nombre de livres
+#   - Si moins de 20 livres, vérifie si moins de 5
+#   - Si moins de 5 livres, affiche le nom de la catégorie
