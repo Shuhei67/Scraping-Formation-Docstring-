66a5fa278fe5b980b1b62a9f600db9d03c32f13d
Lucas <lucas.hoffart2002@gmail.com>
2026-03-21 03:49:36 +0100

    Finalisation du script

--- Part-3/airbnb-scraper/main.py
+++ Part-3/airbnb-scraper/main.py
@@ -1,6 +1,10 @@
 import requests
 from pathlib import Path
+import sys
 import logging
+import re
+from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
 FILEPATH = Path(__file__).parent / "airbnb.html"
 # Path(__file__) = l'emplacement du script
@@ -9,8 +13,9 @@ FILEPATH = Path(__file__).parent / "airbnb.html"
 # Résultat : C:\Users\MonPrénom\Desktop\Scraping Docstring\Part-3\airbnb-scraper\airbnb.html
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
-# Ça permet d'afficher des messages de debug dans le terminal
+logging.basicConfig(level=logging.ERROR)
+# logging.basicConfig(level=logging.DEBUG)
+# Ça permet d'afficher des messages de debug dans le terminal, plus besoin vu que fini
 
 """
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -21,20 +26,59 @@ logging.basicConfig(level=logging.DEBUG)
 # Récupère le contenu de la page à l'URL donnée :
 def fetch_content(url: str, from_disk: bool = False) -> str:
 
+    # Si from_disk = False, alors on va sur internet pour récupérer le contenu
+    # Donc ces 2 lignes ne servent à rien
+    # Par contre si je télécharge je retélécharge pas, je lis le fichier
     if from_disk and FILEPATH.exists():
         return read_from_file()
 
     try:
         logger.debug(f"Récupération du contenu de l'URL : {url}")
-        response = requests.get(url)
-        response.raise_for_status()  # Vérifie que la requête a réussi
-        html_content = response.text
-        write_to_file(content = html_content)
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+            page.goto(url)
+            page.wait_for_selector("[data-testid='card-container']")
+            html_content = page.content()
+            browser.close()
+
+        write_to_file(content=html_content)
         return html_content
-    except requests.RequestException as e:
+    except PlaywrightTimeoutError as e:
+        logger.error(f"Timeout : les annonces n'ont pas chargé : {e}")
+        raise e
+    except Exception as e:
         logger.error(f"Erreur lors de la récupération du contenu : {e}")
         raise e
  
+
+# Traite le contenu HTML pour extraire le prix moyen :
+def get_average_price(html: str) -> int:
+    prices = []
+
+    soup = BeautifulSoup(html, "html.parser")
+    divs = soup.find_all("div", {"data-testid": "card-container"})
+    for div in divs:
+        price_div = div.find("span", class_= "sjwpj0z") or div.find("span", class_="u174bpcy")
+        if not price_div:
+            logger.warning(f"Pas réussi à trouver le prix de la div {div}")
+            continue
+        price = re.sub(r"\D", "", price_div.text)
+        if price.isdigit():
+            logger.debug(f"Prix trouvé : {price}")
+            prices.append(int(price))
+        else:
+            logger.warning(f"Le prix trouvé n'est pas un nombre : {price}")
+    
+    # J'ajoute ces 3 bloc pour que le script soit plus intéressant
+    # Ca n'aide en rien pour le calcul du prix moyen
+    print(f"Nombre d'annonces analysées : {len(prices)}")
+    print(f"Prix le moins cher : {min(prices)}€")
+    print(f"Prix le plus cher : {max(prices)}€")
+
+    return round(sum(prices) / len(prices)) if prices else 0
+
+
 # Écrit le contenu dans un fichier :
 def write_to_file(content: str) -> bool:
     logger.debug(f"Écriture du contenu dans le fichier")
@@ -44,17 +88,18 @@ def write_to_file(content: str) -> bool:
     return FILEPATH.exists()
     # Vérifie si le fichier a été créé avec succès
 
+
 # Lit le contenu du fichier :
 def read_from_file() -> str:
     logger.debug(f"Lecture du contenu du fichier")
     with open(FILEPATH, "r", encoding="utf-8") as f:
         return f.read()
-        
-
-
 
 
 # Ce bloc s'exécute seulement si je lances ce fichier directement
 if __name__ == "__main__":
-    url = "https://www.airbnb.fr/s/Bangkok--Tha%C3%AFlande/homes?refinement_paths%5B%5D=%2Fhomes&acp_id=f766c3c4-071d-469d-9a15-c8760e5d1c99&date_picker_type=monthly_stay&monthly_start_date=2026-04-01&monthly_end_date=2026-05-01&monthly_length=3&adults=1&search_type=user_map_move&flexible_trip_lengths%5B%5D=one_week&price_filter_input_type=1&price_filter_num_nights=30&channel=EXPLORE&zoom_level=11.976609218960075&place_id=ChIJ82ENKDJgHTERIEjiXbIAAQE&query=Bangkok%2C%20Tha%C3%AFlande&search_mode=regular_search&ne_lat=13.826318493439837&ne_lng=100.61760046298133&sw_lat=13.6785445179013&sw_lng=100.50155065953635&zoom=11.976609218960075&search_by_map=true"
-    content = fetch_content(url=url, from_disk=True)
+    url = sys.argv[-1]
+    content = fetch_content(url=url, from_disk=False) # En mettant false il ne lit pas le fichier même s'il existe, il va sur internet
+    average_price = get_average_price(html=content,)
+    print(50 * "-")
+    print(f"Le prix moyen des annonces est de {average_price}€")
